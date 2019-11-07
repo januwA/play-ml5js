4f6bc4938db1476c4ff3f45e02fba566880886bf
januwA <ajanuw1641845087@sina.com>
2019-11-07 20:42:45 +0800

    使用MobileNet进行图像分类

--- image-classification/sketch.js
+++ image-classification/sketch.js
@@ -8,6 +8,7 @@ let labelElem;
 let confidenceElem;
 let inputElem;
 let classifyElem;
+let loadingElem;
 
 function preload() {
   _img = loadImage('https://i.loli.net/2019/11/07/fnbOkChHpl92mig.jpg');
@@ -15,31 +16,36 @@ function preload() {
 
 async function setup() {
   createCanvas(600, 400);
-  background(0);
   labelElem = createP();
   confidenceElem = createP();
+  loadingElem = createP();
   inputElem = select('#input');
   classifyElem = select('#classify');
   classifier = await ml5.imageClassifier('MobileNet');
-
   classifyElem.mouseClicked(classify);
   getResult();
 }
 
 // 获取分类结果
 async function getResult() {
+  loadingElem.html('loading...');
   let r = await classifier.classify(_img);
   console.log(r);
+  loadingElem.html('');
   labelElem.html(r[0].label);
   confidenceElem.html(r[0].confidence);
 }
 
 async function classify() {
   let url = inputElem.value();
-  _img = await loadImage(url);
-  getResult();
+  loadImage(url, img => {
+    _img = img;
+    resizeCanvas(_img.width, _img.height);
+    getResult();
+  });
 }
 
 function draw() {
+  background(0);
   image(_img, 0, 0, width, height);
 }
